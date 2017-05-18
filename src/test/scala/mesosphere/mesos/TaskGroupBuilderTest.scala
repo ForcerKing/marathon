3d1751d4d5c4ce86799233bdfb1cb9b8e6b50820
Johannes Unterstein <junterstein@mesosphere.io>
2017-05-18 21:09:02 +0200

    changed validator

--- src/test/scala/mesosphere/mesos/TaskGroupBuilderTest.scala
+++ src/test/scala/mesosphere/mesos/TaskGroupBuilderTest.scala
@@ -684,7 +684,6 @@ class TaskGroupBuilderTest extends UnitTest with Inside {
             endpoints = Seq(
               raml.Endpoint(
                 name = "foo2",
-                hostPort = Some(1202), // TODO should be changed to random port in this test
                 containerPort = Some(1212)
               )
             )
